c4475e9db8ba50e5154d8ec4e2e0b9f788b31768
darknius09 <darkniuslightness@gmail.com>
2023-03-10 10:36:40 +0000

    Revert "chime: Try to use Greenforce Prebuilt"
    This reverts commit fe39652ecfca82527571d5e4edfb5847aa06f23d.

--- citrus/BoardConfig.mk
+++ citrus/BoardConfig.mk
@@ -13,9 +13,7 @@ include device/xiaomi/chime/BoardConfig-chime.mk
 TARGET_OTA_ASSERT_DEVICE := citrus,chime,juice
 
 # Kernel - Prebuilt images
-BOARD_KERNEL_SEPARATED_DTBO := true
-TARGET_FORCE_PREBUILT_KERNEL := true
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/images/kernel
+#BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/images/dtbo.img
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/images/dtb.img
 BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
